c08058cf274aa1e1876af4cdc2e0a1782751ca36
Jamu Kakar <jkakar@kakar.ca>
2015-05-16 18:59:14 -0700

    Add `exjsx` and `timex` dependencies.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule AWS.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:httpoison, :logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,8 @@ defmodule AWS.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:timex, "~> 0.13.4"}]
+    [{:exjsx, "~> 3.1.0"},
+     {:httpoison, "~> 0.6"},
+     {:timex, "~> 0.13.4"}]
   end
 end

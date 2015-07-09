e59f5d805db72942514aa72d9dde11f37f179e46
Jamu Kakar <jkakar@kakar.ca>
2015-07-09 10:07:18 -0700

    Add clients for the CodeCommit, CodePipeline, DirectoryService and Workspaces services.

--- lib/aws/ecs.ex
+++ lib/aws/ecs.ex
@@ -58,6 +58,21 @@ defmodule AWS.ECS do
   @doc """
   Deregisters an Amazon ECS container instance from the specified cluster.
   This instance will no longer be available to run tasks.
+
+  If you intend to use the container instance for some other purpose after
+  deregistration, you should stop all of the tasks running on the container
+  instance before deregistration to avoid any orphaned tasks from consuming
+  resources.
+
+  Deregistering a container instance removes the instance from a cluster, but
+  it does not terminate the EC2 instance; if you are finished using the
+  instance, be sure to terminate it in the Amazon EC2 console to stop
+  billing.
+
+  <note>When you terminate a container instance, it is automatically
+  deregistered from your cluster.
+
+  </note>
   """
   def deregister_container_instance(client, input, options \\ []) do
     request(client, "DeregisterContainerInstance", input, options)
@@ -265,6 +280,17 @@ defmodule AWS.ECS do
 
   @doc """
   Updates the Amazon ECS container agent on a specified container instance.
+  Updating the Amazon ECS container agent does not interrupt running tasks or
+  services on the container instance. The process for updating the agent
+  differs depending on whether your container instance was launched with the
+  Amazon ECS-optimized AMI or another operating system.
+
+  `UpdateContainerAgent` requires the Amazon ECS-optimized AMI or Amazon
+  Linux with the `ecs-init` service installed and running. For help updating
+  the Amazon ECS container agent on other operating systems, see [Manually
+  Updating the Amazon ECS Container
+  Agent](http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-update.html#manually_update_agent)
+  in the *Amazon EC2 Container Service Developer Guide*.
   """
   def update_container_agent(client, input, options \\ []) do
     request(client, "UpdateContainerAgent", input, options)

3ce95bf6e2eb09464129c51296cade29e27f9822
Akash Manohar J <akash@akash.im>
2016-01-05 09:10:31 +0530

    Update httpoison

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule AWS.Mixfile do
     [{:earmark, "~> 0.1.19", only: :dev},
      {:ex_doc, "~> 0.10", only: :dev},
      {:poison, "~> 1.4.0"},
-     {:httpoison, "~> 0.6"},
+     {:httpoison, "~> 0.8.0"},
      {:timex, "~> 1.0.0"}]
   end
 

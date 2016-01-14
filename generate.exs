36817b12758c1f8aa4bf7cf0563ad8684108da73
Jamu Kakar <jkakar@kakar.ca>
2016-01-13 19:50:29 -0800

    Remove unneeded file.

--- generate.exs
+++
@@ -1,4 +0,0 @@
-[service_description_path, template_path] = System.argv
-context = AWS.Generate.load_context(service_description_path)
-#IO.puts File.read!(template_path)
-IO.puts AWS.Generate.render(context, template_path)

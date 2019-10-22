025b4f6bb378e9290f5d1ce60936a582348de016
azzahidK <48671971+azzahidK@users.noreply.github.com>
2019-10-22 16:12:00 +0700

    Update mix.exs
    Update poison dependency

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule AWS.Mixfile do
      {:earmark, "~> 1.1", only: [:dev]},
      {:ex_doc, "~> 0.15.0", only: [:dev]},
      {:httpoison, "~> 1.5"},
-     {:poison, "~> 3.1"},
+     {:poison, "~> 4.0"},
      {:timex, "~> 3.1"}]
   end
 

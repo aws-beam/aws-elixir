095f4205f558301bb46a80ff6b50319d660debc7
Wojtek Mach <wojtekmach@users.noreply.github.com>
2020-10-22 16:33:57 +0200

    Remove "Docs" link from mix.exs (#39)
    Hex.pm already automatically adds an "Online documentation" link.

--- mix.exs
+++ mix.exs
@@ -45,7 +45,6 @@ defmodule AWS.Mixfile do
     [maintainers: ["Jamu Kakar"],
      licenses: ["Apache 2.0"],
      links: %{"GitHub" => "https://github.com/aws-beam/aws-elixir",
-              "Docs" => "http://hexdocs.pm/aws/#{@version}/",
               "Changelog" => "https://github.com/aws-beam/aws-elixir/blob/master/CHANGELOG.md"}]
   end
 end

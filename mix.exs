c66dfebecec62587dada50602c31c76d307d812c
Philip Sampaio <philip.sampaio@gmail.com>
2021-04-08 11:48:12 -0300

    Revert "Change application name to "aws_elixir" (#66)"
    This reverts commit f2dabe2eacdd66ab4fcd6bce22d84d634658923a.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule AWS.Mixfile do
 
   def project do
     [
-      app: :aws_elixir,
+      app: :aws,
       description: "AWS clients for Elixir",
       package: package(),
       version: @version,
@@ -22,10 +22,22 @@ defmodule AWS.Mixfile do
     ]
   end
 
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
   def application do
     [extra_applications: [:logger, :crypto, :xmerl, :eex]]
   end
 
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:dialyxir, "~> 0.5.0", only: [:dev]},

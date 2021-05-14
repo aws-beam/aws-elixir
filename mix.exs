debdf6482158a71a57636ac664c911e682093395
Philip Sampaio <philip.sampaio@gmail.com>
2021-05-14 17:42:27 -0300

    Update CI to use ErlEF Action (#72)
    It keeps easier to configure and update versions of OTP and Elixir.
    We are using the older supported version and the latest one.
    
    Also, this bumps Elixir requirement to match our build.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule AWS.Mixfile do
       name: "aws-elixir",
       source_url: @repo_url,
       homepage_url: @repo_url,
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       docs: docs(),
@@ -22,22 +22,10 @@ defmodule AWS.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [extra_applications: [:logger, :crypto, :xmerl, :eex]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:dialyxir, "~> 0.5.0", only: [:dev]},

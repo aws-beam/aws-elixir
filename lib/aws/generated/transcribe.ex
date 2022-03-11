434db1ba8de04e608124a9c96d9df10e3fcd403a
GitHub <noreply@github.com>
2022-03-11 01:51:34 +0000

    Update services based on v1.43.16 of AWS Go SDK
    For changes, please check https://github.com/aws/aws-sdk-go/releases/tag/v1.43.16

--- lib/aws/generated/transcribe.ex
+++ lib/aws/generated/transcribe.ex
@@ -26,14 +26,15 @@ defmodule AWS.Transcribe do
   end
 
   @doc """
-  Creates an analytics category.
+  Creates a call analytics category.
 
-  Amazon Transcribe applies the conditions specified by your analytics categories
-  to your call analytics jobs. For each analytics category, you specify one or
-  more rules. For example, you can specify a rule that the customer sentiment was
-  neutral or negative within that category. If you start a call analytics job,
-  Amazon Transcribe applies the category to the analytics job that you've
-  specified.
+  Amazon Transcribe applies the conditions specified by your call analytics
+  categories to your call analytics jobs. For each analytics category, you must
+  create between 1 and 20 rules. For example, you can create a 'greeting' category
+  with a rule that flags calls in which your agent does not use a specified phrase
+  (for example: "Please note this call may be recorded.") in the first 15 seconds
+  of the call. When you start a call analytics job, Amazon Transcribe applies all
+  your existing call analytics categories to that job.
   """
   def create_call_analytics_category(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "CreateCallAnalyticsCategory", input, options)
@@ -42,104 +43,147 @@ defmodule AWS.Transcribe do
   @doc """
   Creates a new custom language model.
 
-  Use Amazon S3 prefixes to provide the location of your input files. The time it
-  takes to create your model depends on the size of your training data.
+  When creating a new language model, you must specify if you want a Wideband
+  (audio sample rates over 16,000 Hz) or Narrowband (audio sample rates under
+  16,000 Hz) base model. You then include the S3 URI location of your training and
+  tuning files, the language for the model, a unique name, and any tags you want
+  associated with your model.
   """
   def create_language_model(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "CreateLanguageModel", input, options)
   end
 
   @doc """
-  Creates a new custom vocabulary that you can use to modify how Amazon Transcribe
-  Medical transcribes your audio file.
+  Creates a new custom medical vocabulary.
+
+  When creating a new medical vocabulary, you must upload a text file that
+  contains your new entries, phrases, and terms into an S3 bucket. Note that this
+  differs from , where you can include a list of terms within your request using
+  the `Phrases` flag, as `CreateMedicalVocabulary` does not support the `Phrases`
+  flag.
+
+  For more information on creating a custom vocabulary text file, see [Creating a custom
+  vocabulary](https://docs.aws.amazon.com/transcribe/latest/dg/custom-vocabulary-create.html).
   """
   def create_medical_vocabulary(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "CreateMedicalVocabulary", input, options)
   end
 
   @doc """
-  Creates a new custom vocabulary that you can use to change the way Amazon
-  Transcribe handles transcription of an audio file.
+  Creates a new custom vocabulary.
+
+  When creating a new medical vocabulary, you can either upload a text file that
+  contains your new entries, phrases, and terms into an S3 bucket or include a
+  list of terms directly in your request using the `Phrases` flag.
+
+  For more information on creating a custom vocabulary, see [Creating a custom vocabulary](https://docs.aws.amazon.com/transcribe/latest/dg/custom-vocabulary-create.html).
   """
   def create_vocabulary(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "CreateVocabulary", input, options)
   end
 
   @doc """
-  Creates a new vocabulary filter that you can use to filter words, such as
-  profane words, from the output of a transcription job.
+  Creates a new vocabulary filter that you can use to filter words from your
+  transcription output.
+
+  For example, you can use this operation to remove profanity from your
+  transcript.
   """
   def create_vocabulary_filter(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "CreateVocabularyFilter", input, options)
   end
 
   @doc """
-  Deletes a call analytics category using its name.
+  Deletes a call analytics category.
+
+  To use this operation, specify the name of the category you want to delete using
+  `CategoryName`.
   """
   def delete_call_analytics_category(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DeleteCallAnalyticsCategory", input, options)
   end
 
   @doc """
-  Deletes a call analytics job using its name.
+  Deletes a call analytics job.
+
+  To use this operation, specify the name of the job you want to delete using
+  `CallAnalyticsJobName`.
   """
   def delete_call_analytics_job(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DeleteCallAnalyticsJob", input, options)
   end
 
   @doc """
-  Deletes a custom language model using its name.
+  Deletes a custom language model.
+
+  To use this operation, specify the name of the language model you want to delete
+  using `ModelName`.
   """
   def delete_language_model(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DeleteLanguageModel", input, options)
   end
 
   @doc """
-  Deletes a transcription job generated by Amazon Transcribe Medical and any
-  related information.
+  Deletes a medical transcription job, along with any related information.
+
+  To use this operation, specify the name of the job you want to delete using
+  `MedicalTranscriptionJobName`.
   """
   def delete_medical_transcription_job(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DeleteMedicalTranscriptionJob", input, options)
   end
 
   @doc """
-  Deletes a vocabulary from Amazon Transcribe Medical.
+  Deletes a custom medical vocabulary.
+
+  To use this operation, specify the name of the vocabulary you want to delete
+  using `VocabularyName`.
   """
   def delete_medical_vocabulary(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DeleteMedicalVocabulary", input, options)
   end
 
   @doc """
-  Deletes a previously submitted transcription job along with any other generated
-  results such as the transcription, models, and so on.
+  Deletes a transcription job, along with any related information.
+
+  To use this operation, specify the name of the job you want to delete using
+  `TranscriptionJobName`.
   """
   def delete_transcription_job(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DeleteTranscriptionJob", input, options)
   end
 
   @doc """
-  Deletes a vocabulary from Amazon Transcribe.
+  Deletes a custom vocabulary.
+
+  To use this operation, specify the name of the vocabulary you want to delete
+  using `VocabularyName`.
   """
   def delete_vocabulary(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DeleteVocabulary", input, options)
   end
 
   @doc """
-  Removes a vocabulary filter.
+  Deletes a vocabulary filter.
+
+  To use this operation, specify the name of the vocabulary filter you want to
+  delete using `VocabularyFilterName`.
   """
   def delete_vocabulary_filter(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DeleteVocabularyFilter", input, options)
   end
 
   @doc """
-  Gets information about a single custom language model.
+  Provides information about a specific custom language model in your Amazon Web
+  Services account.
 
-  Use this information to see details about the language model in your Amazon Web
-  Services account. You can also see whether the base language model used to
-  create your custom language model has been updated. If Amazon Transcribe has
-  updated the base model, you can create a new custom language model using the
-  updated base model. If the language model wasn't created, you can use this
-  operation to understand why Amazon Transcribe couldn't create it.
+  This operation also shows if the base language model you used to create your
+  custom language model has been updated. If Amazon Transcribe has updated the
+  base model, you can create a new custom language model using the updated base
+  model.
+
+  If you tried to create a new custom language model and the request wasn't
+  successful, you can use this operation to help identify the reason.
   """
   def describe_language_model(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "DescribeLanguageModel", input, options)
@@ -153,11 +197,11 @@ defmodule AWS.Transcribe do
   end
 
   @doc """
-  Returns information about a call analytics job.
+  Retrieves information about a call analytics job.
 
-  To see the status of the job, check the `CallAnalyticsJobStatus` field. If the
-  status is `COMPLETED`, the job is finished and you can find the results at the
-  location specified in the `TranscriptFileUri` field. If you enable personally
+  To view the job's status, refer to the `CallAnalyticsJobStatus` field. If the
+  status is `COMPLETED`, the job is finished. You can then find your transcript at
+  the URI specified in the `TranscriptFileUri` field. If you enabled personally
   identifiable information (PII) redaction, the redacted transcript appears in the
   `RedactedTranscriptFileUri` field.
   """
@@ -166,11 +210,11 @@ defmodule AWS.Transcribe do
   end
 
   @doc """
-  Returns information about a transcription job from Amazon Transcribe Medical.
+  Retrieves information about a medical transcription job.
 
-  To see the status of the job, check the `TranscriptionJobStatus` field. If the
-  status is `COMPLETED`, the job is finished. You find the results of the
-  completed job in the `TranscriptFileUri` field.
+  To view the job's status, refer to the `TranscriptionJobStatus` field. If the
+  status is `COMPLETED`, the job is finished. You can then find your transcript at
+  the URI specified in the `TranscriptFileUri` field.
   """
   def get_medical_transcription_job(%Client{} = client, input, options \\ []) do
     Request.request_post(client, metadata(), "GetMedicalTranscriptionJob", input, options)
